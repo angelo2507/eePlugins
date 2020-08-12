eae18cf755ada88bc6df04b188dd3ee35b868e74
Zdzislaw <zdzislaw22@windowslive.com>
2020-08-12 07:25:23 +0200

    poprawka tksm

--- StreamLink/scripts/unofficial/plugins/_tvksm.py
+++ StreamLink/scripts/unofficial/plugins/_tvksm.py
@@ -18,17 +18,19 @@ class tvksm(Plugin):
         return cls._url_re.match(url) is not None
 
     def _get_streams(self):
-        self.session.set_option('hls-live-edge', 10)
+        self.session.http.headers.update({'User-Agent': useragents.CHROME})
         res = self.session.http.get(self.url)
-        #log.debug(res.text)
         
         try:
             address = self._addr_re.search(res.text).group(1)
-            log.debug("Found address: %s" % address)
+            if len(address) == '':
+                log.debug("NO address within response: %s" % res.text)
+            else:
+                log.debug("Found address: %s" % address)
         except Exception as e:
             log.debug(str(e))
             return
         
-        return {"rtsp_stream": FFMPEGMuxer(self.session, *(address,), is_muxed=False)}
+        return {"rtsp_stream": FFMPEGMuxer(self.session, *(address,), is_muxed=False, format='mpegts', vcodec = 'copy', acodec = 'copy')}
 
 __plugin__ = tvksm
